bfb4d7c9f5464aeaf0347f5d7ce5969b767b15b8
Piotr <pplonski86@gmail.com>
2023-08-25 11:43:57 +0200

    set version 0.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="plotai",
-    varsion="0.0.2",
+    version="0.0.1",
     description="Create plots in Python with AI",
     long_description=long_description,
     long_description_content_type="text/markdown",
