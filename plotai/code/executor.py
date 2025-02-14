bdcfb13484f0b85703a4c1ddfd71cb21840e7fde
Piotr <pplonski86@gmail.com>
2025-02-14 15:55:12 +0100

    comment out exec() method because of securith issues

--- plotai/code/executor.py
+++ plotai/code/executor.py
@@ -12,7 +12,12 @@ class Executor:
                 if not line.startswith("```"):
                     tmp_code += line + "\n"
 
-            exec(tmp_code, globals_env, locals_env)
+            # please be aware of security issue with exec functions
+            # LLM can execute arbitrary code
+            # if you are aware of security issues, please uncomment below line
+
+            # exec(tmp_code, globals_env, locals_env)
+            
         except Exception as e:
             return str(e)
         return None
